b9bdff6a69291791de833df3bacd38279b18ed88
Chen Liu <liuchen1701@gmail.com>
2018-04-09 21:41:58 -0700

    Implemented loading html text from instapaper

--- js/blog.js
+++ js/blog.js
@@ -11,6 +11,8 @@ function loadArticle(item, index) {
     var article_header = createDiv("article_header");
     var title = createTitle(item);
     article_header.appendChild(title);
+    var info = createInfo(item);
+    article_header.appendChild(info);
     entry.appendChild(article_header);
 
     var article_content = createDiv("article_content");
@@ -41,6 +43,33 @@ function createTitle(item) {
     return ret;
 }
 
+function createInfo(item) {
+    var ret = document.createElement("h6");
+    ret.setAttribute("class", "info");
+
+    if(item.author === "") {
+        ret.innerHTML += "By Chen Liu @liuchen1701";
+    } else {
+        ret.innerHTML += "转载 Repost";
+    }
+
+    if(item.date === "") {
+
+    } else {
+        ret.innerHTML += "&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;";
+        ret.innerHTML += item.date;
+    }
+
+    if(item.url === "") {
+
+    } else {
+        ret.innerHTML += "&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;";
+        ret.innerHTML += "<a href='"  + item.url + "'>Link to Source</a>";
+    }
+
+    return ret;
+}
+
 function createContent(item) {
     var ret = document.createElement("p");
     ret.setAttribute("class", "main_content");
@@ -48,9 +77,16 @@ function createContent(item) {
     var request = new XMLHttpRequest();
     request.open('GET', item.src, false);
     request.send(null);
-    var text = request.responseText;
 
-    ret.innerText = text;
+    var text;
+
+    if(item.src.includes(".html")) {
+        text = request.responseText;
+   } else {
+        text = request.responseText.replace(new RegExp('\n', 'g'), '<br>');
+    }
+
+    ret.innerHTML = text;
     
     return ret;
 }
