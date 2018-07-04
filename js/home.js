afbd2af39b682a2486beb1061a9253e37c93628b
Chen Liu <liuchen1701@gmail.com>
2018-07-04 16:36:32 -0700

    Remove redundant log

--- js/home.js
+++ js/home.js
@@ -1,4 +1,2 @@
 let index = Math.floor(Math.random() * 4) + 1;
-console.log(index);
 document.getElementsByTagName("header")[0].style.backgroundImage = "url('./resources/images/profile/jpg/Portrait" + index + ".jpg')";
-console.log(document.getElementsByTagName("header")[0].style);
